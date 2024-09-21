074376ebb9823a925b8b93d9f961349ef7bfe4ad
Oleh Yemelianov <emelianov2811@gmail.com>
2024-09-21 17:55:41 +0300

    Task 1 completed

---
+++ task1.py
@@ -0,0 +1,20 @@
+class MyIterator:
+    def __init__(self, num_first, num_end):
+        self.num_first = num_first
+        self.num_end = num_end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num_first < self.num_end:
+            value = self.num_first
+            self.num_first += 1
+            return value
+        else:
+            raise StopIteration
+
+
+new_iter = MyIterator(1, 10)
+for num in new_iter:
+    print(num)
